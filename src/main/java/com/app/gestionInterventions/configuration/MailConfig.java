ba0f23aaaa741cf2dab3fbb9399e67d8ee3397fb
Hichem Ben Hamouda <94752266+BenHamoudaHichem@users.noreply.github.com>
2022-04-21 22:19:00 +0200

    some commit 2

--- src/main/java/com/app/gestionInterventions/configuration/MailConfig.java
+++ src/main/java/com/app/gestionInterventions/configuration/MailConfig.java
@@ -3,6 +3,8 @@ package com.app.gestionInterventions.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 
+import java.math.BigInteger;
+
 public class MailConfig {
     @Bean
     public SimpleMailMessage templateSimpleMessage() {
@@ -10,6 +12,7 @@ public class MailConfig {
         message.setText("Ahmed");
         message.setText(
                 "This is the TEST email template for your email:\n%s\n");
+        BigInteger bigInteger;
         return message;
     }
 }
